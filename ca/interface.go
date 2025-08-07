58d3cd21c115e1bc4555b9d256a7a7796666a5a3
mintfog <fdong26@gmail.com>
2025-08-07 22:32:48 +0800

    mac下证书生成

--- ca/interface.go
+++ ca/interface.go
@@ -1,4 +1,4 @@
-// Copyright 2025 The f-dong Authors
+// Copyright 2025 The mintfog Authors
 // SPDX-License-Identifier: Apache-2.0
 // Use of this source code is governed by an Apache 2.0
 // license that can be found in the LICENSE file.
